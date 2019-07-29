2b9caef381c72ff524cdfc427a3401f629faf54b
n00bertb <53352025+n00bertb@users.noreply.github.com>
2019-07-29 15:17:42 +0300

    Update test_items.py
    добавил коментарии

--- test_items.py
+++ test_items.py
@@ -10,5 +10,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_button(browser):
     browser.get(link)
     time.sleep(5)
+    #проверяем наличие кнопки по ее селектору если ее нет то тест выпадет в ошибку
     button = browser.find_element(By.CSS_SELECTOR,".btn-add-to-basket")
-    ec.presence_of_element_located(browser.find_element(By.CSS_SELECTOR,".btn-add-to-basket"))
+    #можно нажать
+    #button.click()
+    # также можно проверить наличие кнопки так
+    #ec.presence_of_element_located(browser.find_element(By.CSS_SELECTOR,".btn-add-to-basket"))
